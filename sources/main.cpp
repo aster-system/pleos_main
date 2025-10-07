881d0cb9c3f449f3eb379525272ba644f7cce13f
Matt Mmo <mmattmmo@gmail.com>
2025-10-07 08:39:18 +0200

    Add algebra lessons

--- sources/main.cpp
+++ sources/main.cpp
@@ -92,7 +92,7 @@ void draw(std::shared_ptr<scls::Image> img, std::vector<double>& value_1, std::v
 int main(int argc, char* argv[]) {
     //pleos::bac(std::string("tests/"));
 
-    /*pleos::Pleos_Window window(900, 600, argv[0]);
+    pleos::Pleos_Window window(900, 600, argv[0]);
     window.load_from_xml("assets/window.txt");
 
     while(window.run()) {
@@ -102,13 +102,19 @@ int main(int argc, char* argv[]) {
         window.render();
     } //*/
 
-    std::string content = std::string("<h1>Les polynômes</h1>");
+    /*std::string content = std::string("<h1>Les polynômes</h1>");
     std::string redaction = std::string();pleos::polynomial_roots(scls::string_to_formula(std::string("3")), &redaction);content += std::string("<p>") + redaction + std::string("</p>");
     redaction = std::string();pleos::polynomial_roots(scls::string_to_formula(std::string("0")), &redaction);content += std::string("<p>") + redaction + std::string("</p>");
     redaction = std::string();pleos::polynomial_roots(scls::string_to_formula(std::string("8x")), &redaction);content += std::string("<p>") + redaction + std::string("</p>");
     redaction = std::string();pleos::polynomial_roots(scls::string_to_formula(std::string("-3x+7")), &redaction);content += std::string("<p>") + redaction + std::string("</p>");
+    redaction = std::string();pleos::polynomial_roots(scls::string_to_formula(std::string("2x*x-7x+1")), &redaction);content += std::string("<p>") + redaction + std::string("</p>");
+    redaction = std::string();pleos::polynomial_roots(scls::string_to_formula(std::string("x*x-2x+1")), &redaction);content += std::string("<p>") + redaction + std::string("</p>");
+    redaction = std::string();pleos::polynomial_roots(scls::string_to_formula(std::string("4x*x+x+4")), &redaction);content += std::string("<p>") + redaction + std::string("</p>");
     scls::Text_Style style;style.set_max_width(600);
-    pleos::string_to_image(content).save_png("tests/polynome.png");//*/
+    pleos::string_to_image(content, style).save_png("tests/polynome.png");//*/
+
+    //scls::Text_Style style;style.set_font_size(200);
+    //pleos::string_to_image(scls::read_file("tests/test_note.txt"), style).save_png("tests/polynome.png");
 
     return 0;
 }
